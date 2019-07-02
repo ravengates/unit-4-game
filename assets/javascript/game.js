13dc07f49a32ca837910f7c4b3bcdc3269e0e764
Raven Gates <ravengates@icloud.com>
2019-07-02 09:55:46 -0500

    added basic html, added simple psuedocoding before adding code

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//randomly generated number at beginning of game (19-120)
+//4 crystals have a random value (1-12) - not seen. Make an on click for the 4 crystals 
+//once clicked crystal is now added to total score. 
+// once total score is equal to random number, win
+// once total score is over the random numner, loss
